f5843e05fd0c6f42d5c0ccb7930f13b5bb8c39cc
GokhanGuzelyurt <104901443+GokhanGuzelyurt@users.noreply.github.com>
2022-07-15 17:04:15 +0200

    test change

--- src/test/java/com/studocu/utilities/BrowserUtils.java
+++ src/test/java/com/studocu/utilities/BrowserUtils.java
@@ -21,8 +21,8 @@ public class BrowserUtils {
      */
     public static List<String> getElementsText(List<WebElement> list) {
         List<String> elemTexts = new ArrayList<>();
-        for (WebElement el : list) {
-            elemTexts.add(el.getText());
+        for (WebElement ele : list) {
+            elemTexts.add(ele.getText());
         }
         return elemTexts;
     }
